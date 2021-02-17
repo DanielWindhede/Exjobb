d6485c97c1d954f884b2acbb1a3e8547ec742cae
Zmarfan <zmarfan@hotmail.com>
2021-02-17 10:16:36 +0100

    First iteration BezierCurve

---
+++ Assets/VectorGraphics/BezierCurve.cs
@@ -0,0 +1,51 @@
+﻿using Unity.VectorGraphics;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BezierCurve
+{
+    /// <summary>
+    /// Create a bezier curve from a list of points. 
+    /// </summary>
+    /// <param name="points">ordered set of points to create a line</param>
+    /// <param name="curveAmount">How much the curve should curve</param>
+    /// <returns></returns>
+    public static BezierPathSegment[] ConstructBezierCurve(List<Vector2> points, float curveAmount = 0)
+    {
+        BezierPathSegment[] path = new BezierPathSegment[points.Count];
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            //Set the actual point
+            path[i].P0 = points[i];
+            //Outgoing control point from actual point
+            path[i].P1 = CalculateControlPoint(i, true, curveAmount, in points);
+            path[i].P2 = CalculateControlPoint(Modulo(i + 1, points.Count), false, curveAmount, in points);
+        }
+
+        return path;
+    }
+
+    /// <summary>
+    /// Return direction as seen from currentPoint
+    /// </summary>
+    /// <param name="pointIndex">Reference middle point index</param>
+    /// <param name="forward">Should the direction point along the points direction?</param>
+    /// <param name="points">The points</param>
+    static Vector2 CalculateControlPoint(int pointIndex, bool forward, float curveAmount, in List<Vector2> points)
+    {
+        int prePoint = Modulo(pointIndex - 1, points.Count - 1);
+        int nextPoint = Modulo(pointIndex + 1, points.Count - 1);
+        //Pointing toward next point
+        Vector2 direction = (points[nextPoint] - points[prePoint]).normalized;
+        //Reverse if looking backward
+        direction *= forward ? 1 : -1;
+
+        return points[pointIndex] + direction * curveAmount;
+    }
+
+    static int Modulo(int x, int m)
+    {
+        return (x % m + m) % m;
+    }
+}
---
+++ Assets/VectorGraphics/DisplayCurve.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using Unity.VectorGraphics;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DisplayCurve : MonoBehaviour
+{
+    [SerializeField] List<Vector2> _testPoints;
+
+    [Header("Options")]
+
+    [SerializeField, Range(0f, 2f)] float _curveAmount;
+    [SerializeField] Color _color = Color.white;
+    [SerializeField, Range(0.01f, 1f)] float _thickness = 0.1f;
+    [SerializeField] float _test = 1.0f;
+
+    [Header("TessellationOptions")]
+
+    [SerializeField] float _stepDistance = 1000f;
+    [SerializeField] float _maxCordDeviation = 0.05f;
+    [SerializeField] float _maxTanAngleDeviation = 0.05f;
+    [SerializeField] float _samplingStepSize = 0.01f;
+
+    Scene _scene;
+    Shape _path;
+    VectorUtils.TessellationOptions _options;
+    Mesh _mesh;
+
+    private void OnEnable()
+    {
+        SetupOptions();
+        SetupMesh();
+    }
+
+    /// <summary>
+    /// Sets the values for a bezier curve and display it
+    /// </summary>
+    /// <param name="points">Line segments</param>
+    public void SetupCurve(BezierPathSegment[] points)
+    {
+        _path = new Shape()
+        {
+            Contours = new BezierContour[] { new BezierContour() { Segments = points } },
+            PathProps = new PathProperties() { Stroke = new Stroke() { Color = _color, HalfThickness = _thickness } }
+        };
+
+        _scene = new Scene() { Root = new SceneNode() { Shapes = new List<Shape> { _path } } };
+    }
+
+    #region Setup
+
+    /// <summary>
+    /// Setup the TessellationOptions that will persist throughout the playtime
+    /// </summary>
+    void SetupOptions()
+    {
+        _options = new VectorUtils.TessellationOptions()
+        {
+            StepDistance = _stepDistance,
+            MaxCordDeviation = _maxCordDeviation,
+            MaxTanAngleDeviation = _maxTanAngleDeviation,
+            SamplingStepSize = _samplingStepSize
+        };
+    }
+
+    /// <summary>
+    /// Create new mesh that will display line and apply it to meshFilter
+    /// </summary>
+    void SetupMesh()
+    {
+        _mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = _mesh;
+    }
+
+    #endregion
+
+    private void Update()
+    {
+        SetupCurve(BezierCurve.ConstructBezierCurve(_testPoints, _curveAmount));
+
+        if (_path != null)
+            Display();    
+    }
+
+    /// <summary>
+    /// Display line
+    /// </summary>
+    void Display()
+    {
+        List<VectorUtils.Geometry> geometries = VectorUtils.TessellateScene(_scene, _options);
+        VectorUtils.FillMesh(_mesh, geometries, _test);
+    }
+}
