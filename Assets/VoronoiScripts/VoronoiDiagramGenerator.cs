7ea48548bcd4a963f104e78a7f0e628bc6d3a5f7
Zmarfan <zmarfan@hotmail.com>
2021-02-19 15:32:43 +0100

    Comments on voronoi function

--- Assets/VoronoiScripts/VoronoiDiagramGenerator.cs
+++ Assets/VoronoiScripts/VoronoiDiagramGenerator.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class VoronoiDiagramGenerator
 {
+    /// <summary>
+    /// Create a Voronoi graph based of a Delaunay triangulation
+    /// </summary>
+    /// <param name="triangulation">List of all triangles in Delaunay triangulation</param>
+    /// <returns>A traversable graph with points</returns>
     public static VoronoiGraph GenerateVoronoiFromDelaunay(List<Triangle> triangulation)
     {
         List<GraphNode> allNodes = new List<GraphNode>();
         HashSet<Vector2> allPoints = new HashSet<Vector2>();
+
+        //Creates a node from each triangel center and adds points
         foreach (Triangle triangle in triangulation)
         {
             allNodes.Add(new GraphNode(triangle.Center));
@@ -15,11 +22,14 @@ public class VoronoiDiagramGenerator
                 allPoints.Add(point);
         }
 
+        //Connects nodes if triangels share edges
         for (int i = 0; i < triangulation.Count; i++)
         {
+            //Used to skip looping over unnecessary triangels if all connections has been found
             int edgeCounter = 0;
             for (int j = 0; j < triangulation.Count && edgeCounter < 3; j++)
             {
+                //Skip itself
                 if (triangulation[j] == triangulation[i])
                     continue;
 
